d09972b99ea6ab0371c536c2fb15509e71d9ea3f
Kuldeep vishwakarma <kv579678@gmail.com>
2026-08-22 17:30:45 +0530

    Update live backend url

--- prescripto_full-stack_doctor_appointment_app/frontend/src/context/AppContext.jsx
+++ prescripto_full-stack_doctor_appointment_app/frontend/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext()
 const AppContextProvider = (props) => {
 
     const currencySymbol = '₹'
-    const backendUrl = "http://localhost:8001"
+    const backendUrl = "https://doctor-appointment-booking-system-6vea.onrender.com";
 
     const [doctors, setDoctors] = useState([])
     const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '')
